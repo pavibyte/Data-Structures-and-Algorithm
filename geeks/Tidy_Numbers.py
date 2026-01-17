5852e99caa22062957ba2f0201e3be35399fe8c6
L.Pavithra darsini <learnpavi123@gmail.com>
2026-01-17 19:05:24 +0530

    Delete geeks/Tidy_Numbers.py

--- geeks/Tidy_Numbers.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isTidy(self,N):
-        #code here
-        num = str(N)
-        for i in range(len(num)-1):
-            if (int(num[i]) > int(num[i+1]) ):
-                return 0
-        return 1
