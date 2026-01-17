72fa61f8fa8448992ca9ad69a3fd7be934a61701
L.Pavithra darsini <learnpavi123@gmail.com>
2026-01-17 19:05:40 +0530

    Delete geeks directory

--- geeks/find_the_left_over_element.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def leftElement(self,arr) -> int:
-        # code here
-        if len(arr) == 1:
-            return arr[0]
-        arr.sort()
-        return arr[(len(arr)-1)//2]
